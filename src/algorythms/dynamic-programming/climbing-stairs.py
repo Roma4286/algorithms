4f7824afef28043ce451f77b7130e447dcb020f8
Roma4286 <lionkrou8@gmail.com>
2026-09-04 22:37:10 +0300

    climbing-stairs: second way

--- src/algorythms/dynamic-programming/climbing-stairs.py
+++ src/algorythms/dynamic-programming/climbing-stairs.py
@@ -13,3 +13,20 @@ class Solution:
             dp[i] = dp[i - 1] + dp[i - 2]
 
         return dp[n]
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        
+        step1 = 0
+        step2 = 1
+        for i in range(n):
+            curr = step1+step2
+            step1 = step2
+            step2 = curr
+        
+        return curr
