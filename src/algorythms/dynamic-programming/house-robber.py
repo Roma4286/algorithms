bc8caf781943d4cb8fb9d35b90188d01df82c33d
Roma4286 <lionkrou8@gmail.com>
2026-09-05 14:46:08 +0300

    house-robber: second way

--- src/algorythms/dynamic-programming/house-robber.py
+++ src/algorythms/dynamic-programming/house-robber.py
@@ -7,3 +7,14 @@ class Solution:
         for i in range(2, n+1):
             dp[i] = max(dp[i - 1], dp[i - 2] + nums[i-1])
         return dp[n]
+
+class Solution:
+    def rob(self, nums: list[int]) -> int:
+        n = len(nums)
+        step1 = 0
+        step2 = nums[0]
+        for i in range(2, n + 1):
+            curr = max(step2, step1 + nums[i-1])
+            step1 = step2
+            step2 = curr
+        return curr
